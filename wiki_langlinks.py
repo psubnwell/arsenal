cd8577da845c0d682232cd90d56b247c200c1ede
Dongyao Hu <hu.dongyao.01@gmail.com>
2017-05-06 23:55:45 +0800

    add extract_title() and translate_title()

--- wiki_langlinks.py
+++ wiki_langlinks.py
@@ -1,24 +1,49 @@
 import os
 import re
+import langid
 
-def extract_specific_language(langlinks, language):
-    item = re.findall(r'\((\d+),\'(' + language + r')\',\'(.*?)\'\)', langlinks)
+def extract_language_code(langlinks, language_code):
+    item = re.findall(r'\((\d+),\'(' + language_code + r')\',\'(.*?)\'\)', langlinks)
     item_nonempty = []
     for i in item:
         if i[2] != '':
             item_nonempty.append(i)
-    return item_nonempty[:10]
+    return item_nonempty
 
-def extract_specific_identity(langlinks, identity):
-    item = re.findall(r'\((' + str(identity) + r'),\'(.+?)\',\'(.*?)\'\)', langlinks)
+def extract_page_id(langlinks, page_id):
+    item = re.findall(r'\((' + str(page_id) + r'),\'(.+?)\',\'(.*?)\'\)', langlinks)
     item_nonempty = []
     for i in item:
         if i[2] != '':
             item_nonempty.append(i)
-    return item_nonempty[:10]
+    return item_nonempty
+
+def extract_title(langlinks, title):
+    import langid
+    language_code = langid.classify(title)[0]
+    item = extract_language_code(langlinks, language_code)
+    if language_code == 'zh':
+        import opencc
+        title_zhs = opencc.convert(title, config='zht2zhs.ini')
+        title_zht = opencc.convert(title, config='zhs2zht.ini')
+        return [i for i in item if (i[2] == title_zhs or i[2] == title_zht)]
+    else:
+        return [i for i in item if i[2] == title]
+
+def translate_title(langlinks, title, target_language_code='all'):
+    # import langid
+    # language_code = langid.classify(title)[0]
+    page_id = extract_title(langlinks, title)[0][0]
+    item = extract_page_id(langlinks, page_id)
+    if target_language_code == 'all':
+        return item
+    else:
+        return [i for i in item if i[1] == target_language_code]
 
 if __name__ == '__main__':
-    langlinks = open('./zhwiki-20170501-langlinks.sql', errors='ignore').read()
-        # The langlinks file is not pure UTF-8, so it's better to ignore the error.
-    print(extract_specific_language(langlinks=langlinks, language='en')[:10])
-    print(extract_specific_identity(langlinks=langlinks, identity=7662)[:10])
+    langlinks_zh = open('./zhwiki-20170501-langlinks.sql', errors='ignore').read()
+    langlinks_en = open('./enwiki-20170501-langlinks.sql', errors='ignore').read()
+        # The langlinks fpipile is not pure UTF-8, so it's better to ignore the error.
+    print(extract_language_code(langlinks_zh, 'en')[:10])
+    print(extract_page_id(langlinks_zh, 7662)[:10])
+    print(translate_title(langlinks_ru, '国立交通大学')[:30])

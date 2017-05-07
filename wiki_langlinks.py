b01cf93ee57b11aa004fe03a7d3db815621fed8a
Dongyao Hu <hu.dongyao.01@gmail.com>
2017-05-07 14:56:04 +0800

    add id2title() and title2id()

--- wiki_langlinks.py
+++ wiki_langlinks.py
@@ -40,10 +40,33 @@ def translate_title(langlinks, title, target_language_code='all'):
     else:
         return [i for i in item if i[1] == target_language_code]
 
+def id2title(pages_articles_multistream_index_txt, page_id):
+    item = re.findall(r'\d+:' + str(page_id) + r':(.*)', 
+        pages_articles_multistream_index_txt)
+    return item[0]
+
+def title2id(pages_articles_multistream_index_txt, page_title):
+    import langid
+    language_code = langid.classify(page_title)[0]
+    if language_code == 'zh':
+        import opencc
+        page_title_zhs = opencc.convert(page_title, config='zht2zhs.ini')
+        page_title_zht = opencc.convert(page_title, config='zht2zhs.ini')
+        item = re.findall(r'\d+:(\d+):({0}|{1})\n'.format(page_title_zhs, page_title_zht), 
+            pages_articles_multistream_index_txt)
+    else:
+        item = re.findall(r'\d+:(\d+):({0})'.format(page_title), 
+            pages_articles_multistream_index_txt)
+    return item
+
 if __name__ == '__main__':
     langlinks_zh = open('./zhwiki-20170501-langlinks.sql', errors='ignore').read()
     langlinks_en = open('./enwiki-20170501-langlinks.sql', errors='ignore').read()
+    index_zh = open('./zhwiki-20170501-pages-articles-multistream-index.txt', errors='ignore').read()
         # The langlinks fpipile is not pure UTF-8, so it's better to ignore the error.
     print(extract_language_code(langlinks_zh, 'en')[:10])
     print(extract_page_id(langlinks_zh, 7662)[:10])
-    print(translate_title(langlinks_ru, '国立交通大学')[:30])
+    print(extract_title(langlinks_en, '哈尔滨工业大学'))
+    print(translate_title(langlinks_de, '国立交通大学'))
+    print(id2title(index_zh, 425))
+    print(title2id(index_zh, '浙江省'))

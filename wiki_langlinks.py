c62bc847990bcf3daa696e7363cbb79a7ff8d076
Dongyao Hu <hu.dongyao.01@gmail.com>
2017-05-09 22:06:52 +0800

    modify chinese_convert()

--- wiki_langlinks.py
+++ wiki_langlinks.py
@@ -1,34 +1,52 @@
 import os
 import re
+import json
 import langid
 import opencc
 
+CHINESE_CONVERT_CONFIG = 'zht2zhs.ini'
+
+def chinese_convert(foo, config=CHINESE_CONVERT_CONFIG):
+    if type(foo) == 'str':  # Foo is a string.    
+        return opencc.convert(foo, config=config)
+    else:  # Foo is other data structure, such as list, tuple.
+        tmp = json.dumps(foo, ensure_ascii=False)
+        tmp = opencc.convert(tmp, config=config)
+        return json.loads(tmp)
+
 def extract_language(langlinks_sql, language_code):
     item = re.findall(r'\((\d+),\'(' + language_code + r')\',\'(.*?)\'\)', langlinks_sql)
+
     item_nonempty = []
     for i in item:
         if i[2] != '':
             item_nonempty.append(i)
+
+    if language_code == 'zh':
+        item_nonempty = chinese_convert(item_nonempty)
     return item_nonempty
 
 def extract_id(langlinks_sql, page_id):
     item = re.findall(r'\((' + str(page_id) + r'),\'(.+?)\',\'(.*?)\'\)', langlinks_sql)
+
     item_nonempty = []
     for i in item:
         if i[2] != '':
             item_nonempty.append(i)
-    return item_nonempty
 
-def extract_title(langlinks_sql, title):
-    import langid
-    language_code = langid.classify(title)[0]
-    item = extract_language(langlinks_sql, language_code)
+    for i in range(len(item)):
+        if item[i][1] == 'zh':
+            item[i] = chinese_convert(item[i])
+    return item
+
+def extract_title(langlinks_sql, page_title, language_code=None):
+    if language_code == None:
+        language_code = langid.classify(page_title)[0]
     if language_code == 'zh':
-        title_zhs = opencc.convert(title, config='zht2zhs.ini')
-        title_zht = opencc.convert(title, config='zhs2zht.ini')
-        return [i for i in item if (i[2] == title_zhs or i[2] == title_zht)]
-    else:
-        return [i for i in item if i[2] == title]
+        page_title = chinese_convert(page_title)
+
+    item = extract_language(langlinks_sql, language_code)
+    return [i for i in item if i[2] == page_title]
 
 def translate_title(langlinks_sql, title, target_language_code='all'):
     # import langid
@@ -64,15 +82,19 @@ def read_wiki(file_path):
         f = opencc.convert(f, config='zht2zhs.ini')
     return f
 
+
+
+
 if __name__ == '__main__':
-    langlinks_zh = read_wiki('./zhwiki-20170501-langlinks.sql')
+    # The wiki file is not pure UTF-8, so it's better to ignore the error.
+    langlinks_zh = open('./zhwiki-20170501-langlinks.sql', errors='ignore').read()
+    index_zh = open('./zhwiki-20170501-pages-articles-multistream-index.txt', 
+        errors='ignore').read()
 
     langlinks_en = open('./enwiki-20170501-langlinks.sql', errors='ignore').read()
-    index_zh = open('./zhwiki-20170501-pages-articles-multistream-index.txt', errors='ignore').read()
-    index_zhs = opencc.convert(index_zh, config='zht2zhs.ini')
-        
-    print(extract_language(langlinks_zhs, 'en')[:10])
-    print(extract_id(langlinks_zhs, 7662)[:10])
+
+    print(extract_language(langlinks_zh, 'en')[:10])
+    print(extract_id(langlinks_zh, 7662)[:10])
     print(extract_title(langlinks_en, '哈尔滨工业大学'))
     print(translate_title(langlinks_de, '国立交通大学'))
     print(id2title(index_zh, 425))
